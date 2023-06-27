f82a0c18d9fc7b9a943918b8933becd030653b9d
Anna Edmund <annecreeyates@gmail.com>
2023-06-27 23:28:56 +0100

    In index.html: added Pause and Stop button; in main.js: connected the 3 buttons - start, pause and stop, added the functionality for the audio capture - there is a bug-AudioContext was not allowed to start; Needs to be resumed.

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
 // Connect the Check button
-const checkBtn = document.getElementById("gauge-button");
-const print = () => {
-  console.log("Button works!");
-};
-
-checkBtn.addEventListener("click", print);
+const startBtn = document.getElementById("gauge_start-btn");
+const pauseBtn = document.getElementById("gauge_pause-btn");
+const stopBtn = document.getElementById("gauge_stop-btn");
 
 // AUDIO CAPTURING
 // Create an audio context
+const audioContext = new AudioContext();
 
-// Access the user's microphone
+// Function starting the measurement
+startBtn.addEventListener("click", () => {
+  // Access the user's microphone
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => {
+      // Create a media stream source node from the microphone stream
+      const microphoneSource = audioContext.createMediaStreamSource(stream);
+      // Create the analyser node
+      const analyserNode = audioContext.createAnalyser();
+      microphoneSource.connect(analyserNode);
+      // Set up analyser node configuration
+      analyserNode.fftSize = 2048;
+      const bufferLength = analyserNode.frequencyBinCount;
+      const dataArray = new Float32Array(bufferLength);
 
-// Create a media stream source node from the microphone stream
+      // Create a function to process audio data and calculate noise levels
+      const processAudio = () => {
+        // Get audio data from the analyser node
+        analyserNode.getFloatFrequencyData(dataArray);
 
-// Create the analyser node
+        // Calculate the root mean square (RMS) of the audio data
+        let sumOfsquares = 0;
+        for (let i = 0; i < bufferLength; i++) {
+          const amplitude = dataArray[i];
+          sumOfsquares += amplitude * amplitude;
+        }
+        const rms = Math.sqrt(sumOfsquares / bufferLength);
 
-// Set up analyser node configuration
+        // Covert RMS to decibels (dB)
+        const reference = 1; // Reference amplitude
+        const decibels = 20 * Math.log10(rms / reference);
 
-// Create a function to process audio data and calculate noise levels
+        // Updata the UI
+        console.log(decibels);
+        // Call the processAudio function to continuously process audio data
+        requestAnimationFrame(processAudio);
+      };
+      // Start processing audio data
+      processAudio();
+    })
+    // Handle error
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-// Get audio data from the analyser node
+// Function pausing the capture
+pauseBtn.addEventListener("click", () => {
+  // Pause the audio capture
+  audioContext.suspend();
+});
 
-// Calculate the root mean square (RMS) of the audio data
-
-// Covert RMS to decibels (dB)
-
-// Updata the UI
-
-// Call the processAudio function to continuously process audio data
-
-// Start processing audio data
-
-// Handle error
+// Function stopping the capture
+stopBtn.addEventListener("click", () => {
+  // Stop the audio capture
+  audioContext.close();
+});
