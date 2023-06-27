c584fd1b3b0a022c04a7c2f79c88418f1c378a80
Anna Edmund <annecreeyates@gmail.com>
2023-06-27 22:38:53 +0100

    Created basic layout in index.html; added button event listener and instructions for the noise analyser implementation

---
+++ main.js
@@ -0,0 +1,34 @@
+// Connect the Check button
+const checkBtn = document.getElementById("gauge-button");
+const print = () => {
+  console.log("Button works!");
+};
+
+checkBtn.addEventListener("click", print);
+
+// AUDIO CAPTURING
+// Create an audio context
+
+// Access the user's microphone
+
+// Create a media stream source node from the microphone stream
+
+// Create the analyser node
+
+// Set up analyser node configuration
+
+// Create a function to process audio data and calculate noise levels
+
+// Get audio data from the analyser node
+
+// Calculate the root mean square (RMS) of the audio data
+
+// Covert RMS to decibels (dB)
+
+// Updata the UI
+
+// Call the processAudio function to continuously process audio data
+
+// Start processing audio data
+
+// Handle error
